fd7ba55a29051bd4b7ae0215b14856754ee34248
OrionGift3 <168460082+Shengelaa@users.noreply.github.com>
2024-12-18 18:12:39 +0400

    rrr

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import './App.css';
-import Header from './component/Header';
-import Invoices from './component/Invoices';
-import SideBar from './component/sideBar';
-import SideAdd from './component/SideAdd';
-import EmptyBanner from './component/EmptyBanner';
-import { useState } from 'react';
+import "./App.css";
+import Header from "./component/Header";
+import Invoices from "./component/Invoices";
+import SideBar from "./component/sideBar";
+import SideAdd from "./component/SideAdd";
+import EmptyBanner from "./component/EmptyBanner";
+import { useState } from "react";
+import SideEdit from "./component/SideEdit";
 
 function App() {
   const [isInvoces, setIsInvoces] = useState(false);
@@ -12,9 +13,10 @@ function App() {
     <>
       <div className='relative w-full'>
         <div className='fixed left-0 top-0'>{<SideBar />}</div>
+        <div>{<SideEdit />}</div>
         <div className='m-auto w-[60%]'>
-          <header className='mb-7'>{<Header />}</header>
-          <main>{isInvoces ? <Invoices /> : <EmptyBanner />}</main>
+          {/* <header className='mb-7'>{<Header />}</header>
+          <main>{isInvoces ? <Invoices /> : <EmptyBanner />}</main> */}
         </div>
       </div>
     </>
--- src/component/SideEdit.jsx
+++ src/component/SideEdit.jsx
@@ -1,7 +1,105 @@
-import React from 'react';
+import React from "react";
 
 const SideEdit = () => {
-  return <div>SideEdit</div>;
+  return (
+    <div className='w-[719px] h-screen  pl-[159px] pt-[59px] pr-[56px] pb-[32px]'>
+      <div className='flex flex-col '>
+        <div className='mb-[46px]'>
+          <h1 className='text-[#0C0E16] text-[24px] font-bold  '>
+            Edit #XM9141
+          </h1>
+        </div>
+        <div>
+          <p className='text-[#7C5DFA] font-bold mb-[24px]'>Bill From</p>
+          <p className='text-[#7E88C3] font-medium mb-[9px]'>Street Adress</p>
+          <input
+            type='text'
+            placeholder='19 Union Terrance'
+            className='w-[504px] h-[48px] border border-[#DFE3FA] rounded mb-[25px]'
+          />
+          <div className='flex flex-row mb-[49px]'>
+            <div>
+              <p className='text-[#7E88C3] font-medium mb-[9px]'>City</p>
+              <input
+                type='text'
+                placeholder='London'
+                className='w-[152px] h-[48px]  border border-[#DFE3FA] rounded mb-[25px]'
+              />
+            </div>
+            <div>
+              <p className='text-[#7E88C3] ml-[24px] font-medium mb-[9px]'>
+                Post Code
+              </p>
+              <input
+                type='text'
+                placeholder='E1 3EZ'
+                className='w-[152px] h-[48px] ml-[24px] border border-[#DFE3FA] rounded mb-[25px]'
+              />
+            </div>
+            <div>
+              <p className='text-[#7E88C3] ml-[24px] font-medium mb-[9px]'>
+                Country
+              </p>
+              <input
+                type='text'
+                placeholder='United Kingdom'
+                className='w-[152px] ml-[24px] h-[48px] border border-[#DFE3FA] rounded mb-[25px]'
+              />
+            </div>
+          </div>
+          <p className='text-[#7C5DFA] font-bold mb-[24px]'>Bill To</p>
+          <p className='text-[#7E88C3] font-medium mb-[9px]'>Clients Name</p>
+          <input
+            type='text'
+            placeholder='Alex Grim'
+            className='w-[504px] h-[48px] border border-[#DFE3FA] rounded mb-[25px]'
+          />
+          <p className='text-[#7E88C3] font-medium mb-[9px]'>Clients Email</p>
+          <input
+            type='text'
+            placeholder='Alexgrim@gmail.com'
+            className='w-[504px] h-[48px] border border-[#DFE3FA] rounded mb-[25px]'
+          />
+          <p className='text-[#7E88C3] font-medium mb-[9px]'>Street Adress</p>
+          <input
+            type='text'
+            placeholder='84 Church way'
+            className='w-[504px] h-[48px] border border-[#DFE3FA] rounded mb-[25px]'
+          />
+          <div className='flex flex-row mb-[49px]'>
+            <div>
+              <p className='text-[#7E88C3] font-medium mb-[9px]'>City</p>
+              <input
+                type='text'
+                placeholder='BradFord'
+                className='w-[152px] h-[48px]  border border-[#DFE3FA] rounded mb-[25px]'
+              />
+            </div>
+            <div>
+              <p className='text-[#7E88C3] ml-[24px] font-medium mb-[9px]'>
+                Post Code
+              </p>
+              <input
+                type='text'
+                placeholder='BD1 9PB'
+                className='w-[152px] h-[48px] ml-[24px] border border-[#DFE3FA] rounded mb-[25px]'
+              />
+            </div>
+            <div>
+              <p className='text-[#7E88C3] ml-[24px] font-medium mb-[9px]'>
+                Country
+              </p>
+              <input
+                type='text'
+                placeholder='United Kingdom'
+                className='w-[152px] ml-[24px] h-[48px] border border-[#DFE3FA] rounded mb-[25px]'
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SideEdit;
