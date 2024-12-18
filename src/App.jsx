ad598bfdb3e4e912698de3d1fafa0aa0f0ca5896
kupre1 <160654812+kupre1@users.noreply.github.com>
2024-12-18 19:45:25 +0400

    feat/created:header

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import './App.css';
-import Header from './component/Header';
-import Invoices from './component/Invoices';
-import SideBar from './component/sideBar';
-import SideAdd from './component/SideAdd';
+import "./App.css";
+import Header from "./component/Header";
+import Invoices from "./component/Invoices";
+import SideBar from "./component/sideBar";
+import SideAdd from "./component/SideAdd";
 
 function App() {
   return (
     <>
-      <div className='relative w-full'>
-        <div className='fixed left-0 top-0'>{<SideBar />}</div>
-        <div className='m-auto w-[60%]'>
-          <header className='mb-7'>{<Header />}</header>
+      <div className="relative w-full">
+        <div className="fixed left-0 top-0">{<SideBar />}</div>
+        <div className="m-auto w-[60%]">
+          <header className="mb-7">{<Header />}</header>
           <main>{<Invoices />}</main>
         </div>
       </div>
