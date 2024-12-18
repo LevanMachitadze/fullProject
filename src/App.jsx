1218b8abefb49d147c502b419153d5a06608ccf5
Levani <machitadzelevan@gmail.com>
2024-12-18 17:43:30 +0400

    feat/empty
    creating empty main content

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,18 @@ import Header from './component/Header';
 import Invoices from './component/Invoices';
 import SideBar from './component/sideBar';
 import SideAdd from './component/SideAdd';
+import EmptyBanner from './component/EmptyBanner';
+import { useState } from 'react';
 
 function App() {
+  const [isInvoces, setIsInvoces] = useState(false);
   return (
     <>
       <div className='relative w-full'>
         <div className='fixed left-0 top-0'>{<SideBar />}</div>
         <div className='m-auto w-[60%]'>
           <header className='mb-7'>{<Header />}</header>
-          <main>{<Invoices />}</main>
+          <main>{isInvoces ? <Invoices /> : <EmptyBanner />}</main>
         </div>
       </div>
     </>
