17b2a12128982667ee388493cd6a2be9bafae62d
Levani <machitadzelevan@gmail.com>
2024-12-21 23:59:34 +0400

    feat/delete
    adding functional delete invoice.
    adding functional edit invoice.
    adding delete alert

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-
 import React, { useState } from 'react';
 import './App.css';
 import Header from './component/Header';
@@ -11,7 +10,6 @@ import AddInvoice from './component/SideAdd';
 const App = () => {
   const [isInvoices, setIsInvoices] = useState(true);
 
-
   return (
     <Router>
       <div className='relative w-full'>
@@ -21,22 +19,10 @@ const App = () => {
           <main>
             <Routes>
               <Route path='/' element={<Invoices />} />
-              <Route path='/sideedit/:id' element={<AddInvoice />} />
+              <Route path='/sideadd/:id' element={<AddInvoice />} />
               <Route path='/empty' element={<EmptyBanner />} />
             </Routes>
           </main>
-
-function App() {
-  const [isInvoces, setIsInvoces] = useState(true);
-  return (
-    <>
-
-      <div className="relative w-full">
-        <div className="fixed left-0 top-0">{<SideBar />}</div>
-        <div className="m-auto w-[60%]">
-          <header className="mb-7">{<Header />}</header>
-          <main>{<Invoices />}</main>
-
         </div>
       </div>
     </Router>
--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
-import upArrow from "../assets/png/uparrow.png";
-import plius from "../assets/png/plius.png";
+import React, { useState } from 'react';
+import upArrow from '../assets/png/uparrow.png';
+import plius from '../assets/png/plius.png';
 
 const Header = () => {
   const [isFilterOpen, setIsFilterOpen] = useState(false);
@@ -22,54 +22,54 @@ const Header = () => {
   };
 
   return (
-    <header className="flex justify-between items-center">
+    <header className='flex justify-between items-center flex-col sm:flex-row text-center'>
       <div>
         <div>
-          <p className="text-[#0C0E16] text-[36px] font-bold">Invoices</p>
-          <p className="text-[#888EB0]">There are 3 pending invoices</p>
+          <p className='text-[#0C0E16] text-[36px] font-bold'>Invoices</p>
+          <p className='text-[#888EB0]'>There are 34 pending invoices</p>
         </div>
       </div>
-      <div className="flex items-center gap-9">
+      <div className='flex items-center gap-9 flex-col sm:flex-row'>
         <div>
           <p
-            className="flex text-[#0C0E16] items-center gap-2 text-[15px] font-bold leading-[15px]"
+            className='flex text-[#0C0E16] items-center gap-2 text-[15px] font-bold leading-[15px]'
             onClick={toggleFilter}
           >
-            Filter by status{" "}
-            <img src={upArrow} className="w-[8px] h-[4px]" alt="arrow" />
+            Filter by status{' '}
+            <img src={upArrow} className='w-[8px] h-[4px]' alt='arrow' />
           </p>
 
           {isFilterOpen && (
-            <div className="absolute bg-white w-[192px] h-[128px] mt-6 shadow-lg rounded-md flex flex-col">
+            <div className='absolute bg-white w-[192px] h-[128px] mt-6 shadow-lg rounded-md flex flex-col'>
               {!checkedStatus.pending && !checkedStatus.completed && (
-                <label className="p-2 text-[#0C0E16]">
+                <label className='p-2 text-[#0C0E16]'>
                   <input
-                    type="checkbox"
-                    className="mr-2"
+                    type='checkbox'
+                    className='mr-2'
                     checked={checkedStatus.draft}
-                    onChange={() => handleCheckboxChange("draft")}
+                    onChange={() => handleCheckboxChange('draft')}
                   />
                   Draft
                 </label>
               )}
               {!checkedStatus.draft && !checkedStatus.completed && (
-                <label className="p-2 text-[#0C0E16]">
+                <label className='p-2 text-[#0C0E16]'>
                   <input
-                    type="checkbox"
-                    className="mr-2"
+                    type='checkbox'
+                    className='mr-2'
                     checked={checkedStatus.pending}
-                    onChange={() => handleCheckboxChange("pending")}
+                    onChange={() => handleCheckboxChange('pending')}
                   />
                   Pending
                 </label>
               )}
               {!checkedStatus.draft && !checkedStatus.pending && (
-                <label className="p-2 text-[#0C0E16]">
+                <label className='p-2 text-[#0C0E16]'>
                   <input
-                    type="checkbox"
-                    className="mr-2"
+                    type='checkbox'
+                    className='mr-2'
                     checked={checkedStatus.completed}
-                    onChange={() => handleCheckboxChange("completed")}
+                    onChange={() => handleCheckboxChange('completed')}
                   />
                   Completed
                 </label>
@@ -78,11 +78,11 @@ const Header = () => {
           )}
         </div>
 
-        <button className="w-[190px] h-[50px] pl-3 bg-[#9277FF] leading-[33px] rounded-[50px] flex items-center gap-5">
-          <div className="w-[40px] h-[40px] bg-white flex items-center justify-center rounded-3xl">
-            <img src={plius} alt="plius" />
+        <button className='w-[190px] h-[50px] pl-3 bg-[#9277FF] leading-[33px] rounded-[50px] flex items-center gap-5'>
+          <div className='w-[40px] h-[40px] bg-white flex items-center justify-center rounded-3xl'>
+            <img src={plius} alt='plius' />
           </div>
-          <p className="text-white text-[15px] font-bold leading-[15px]">
+          <p className='text-white text-[15px] font-bold leading-[15px]'>
             New Invoice
           </p>
         </button>
